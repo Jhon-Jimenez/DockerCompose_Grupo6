c86494b74c58a1a0041cfe48fea0d582e60c77c4
sergio110304 <slcabana@uninorte.edu.co>
2024-08-15 07:50:06 -0500

    Contenedor 2

--- app1/app_web.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Aplicación Web - Contenedor 2"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
